751db3e23e7d272d78680a10614ece719babac3e
tomas <tomas.tini07@gmail.com>
2025-03-16 22:58:23 -0300

    optimizacion ejercicio 1

--- Guia_1/Ejercicio_1.py
+++ Guia_1/Ejercicio_1.py
@@ -1,14 +1,15 @@
 lista = [8,12,9,45]
-posicion = -1
-num_encontrado = 0
+posicion = 0
+num_encontrado = False
 valor_ingresado = input("ingrese un valor de la lista [8,12,9,45]: ")
 valor_ingresado_numero = int(valor_ingresado)
 for elemento in lista:
-    posicion += 1
+    
     if elemento == valor_ingresado_numero:
-        num_encontrado = 1
+        num_encontrado = True
         print("El  numero se encuentra en la posicion ", posicion)
-if not num_encontrado:
+    posicion += 1
+if num_encontrado == False:
         print("El numero no esta en la lista")
         breakpoint
     
