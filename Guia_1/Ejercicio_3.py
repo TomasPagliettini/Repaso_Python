1cbfdc77ac5553dbb529a31ab9b804b7a60cf15d
tomas <tomas.tini07@gmail.com>
2025-03-16 23:24:59 -0300

    cambio ejercicio 3

--- Guia_1/Ejercicio_3.py
+++ Guia_1/Ejercicio_3.py
@@ -2,6 +2,6 @@ ancho = int(input("Ingrese el ancho: "))
 alto = int(input("Ingrese el alto: "))
 caracter_a_usar = input("Ingrese el caracter con el que se va a rellenar el rectangulo: ")
 
-for _ in range(alto):
+for a in range(alto):
     print((caracter_a_usar + " ")*ancho)
     
